5d22df20c54b3ffddf70f36c777f4aaa8e19eda5
xwwwwww <wenjx_thu@163.com>
2021-08-27 15:14:50 +0000

    udpate

--- finetune_deepspeed.sh
+++ finetune_deepspeed.sh
@@ -2,11 +2,16 @@
 SIZE=large
 DATA_PATH=esc
 # DATA_PATH=data
-SAVE_DIR=test_${SIZE}_${DATA_PATH}_deepspeed
+SAVE_DIR=lot_${SIZE}_${DATA_PATH}_deepspeed
+
+NUM_WORKERS=1
+NUM_GPUS_PER_WORKER=8
+HOST=m0
+HOST_FILE=config/hostfile/hostfile-$HOST
 
 
 export NCCL_DEBUG=INFO
-deepspeed  \
+/opt/conda/bin/deepspeed --num_nodes ${NUM_WORKERS} --num_gpus ${NUM_GPUS_PER_WORKER} --master_port 4586 --hostfile ${HOST_FILE} \
     code/finetune_trainer.py \
     --data_dir=$DATA_PATH \
     --train_name=train \
@@ -29,4 +34,4 @@ deepspeed  \
     --overwrite_output_dir \
     --load_best_model_at_end \
     --gradient_accumulation_steps 40 \
-    --deepspeed config/ds_zero2_config_st.json
+    --deepspeed config/deepspeed/ds_zero2_config_st.json
