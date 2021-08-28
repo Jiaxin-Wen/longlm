1a32bccab9b49cb263f787294e63682afec02c13
root <root@notebook46a501eb59-jupyter-master-0.notebook46a501eb59-jupyter-master.dev-jhmtvbr7.svc.cluster.local>
2021-08-28 04:30:28 +0000

    update infer

--- code/infer.py
+++ code/infer.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import json
 import argparse
 from unicodedata import category
 
@@ -66,14 +68,21 @@ def generate(args):
     print(f"loaded model from : {args.load_path}")
 
     # load data
-    with open(args.input_path, 'r', encoding='utf-8') as f:
+    with open(args.input_path + '.source', 'r', encoding='utf-8') as f:
         input = [line.strip() for line in f.readlines()]
-    print(f"loaded input data from : {args.input_path}")
+    print(f"loaded input data from : {args.input_path + '.source'}")
+    with open(args.input_path + '.target', 'r', encoding='utf-8') as f:
+        target = [line.strip() for line in f.readlines()]
+    print(f"loaded input data from : {args.input_pat + '.target'}")
 
 
     print(f'start generate to : {args.output_path}')
+    if not os.path.exists(args.output_path):
+        os.mkdir(args.output_path)
+
     start, end = 0, 0
-    with open(args.output_path, 'w', encoding='utf-8') as f:
+    generate = []
+    with open(args.output_path + 'generate.txt', 'w', encoding='utf-8') as f:
         with torch.no_grad():
             while end < len(input):
                 start, end = end, min(end + args.batch_size, len(input))
@@ -82,14 +91,19 @@ def generate(args):
                 dec_input_ids = torch.tensor([tokenizer.convert_tokens_to_ids(tokenizer.tokenize(dec_input)) for _ in range(end-start)], dtype=torch.long).to(args.device)
                 gen = model.generate(enc_input_ids, decoder_input_ids=dec_input_ids, do_sample=True, max_length=args.dec_seq_length, top_p=0.9, temperature=0.7, decoder_start_token_id=1)
                 gen = [process(i, tokenizer) for i in gen]
+                generate += gen
                 print(f'start = {start}, end = {end}')
 
                 for output in gen:
                     f.write(output)
                     f.write('\n')
                 f.flush()
-            
-            
+
+
+    # TODO: cal metric
+    with open(args.output_path + 'metric.json', 'w', encoding='utf-8') as f:
+        pass
+    
 
 if __name__ == "__main__":
     args = get_args()
