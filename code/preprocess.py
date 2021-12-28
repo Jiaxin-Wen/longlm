e12943e4490cdc7a045e3a2eaef62401496840d6
root <root@notebook31559ec93d-jupyter-master-0.notebook31559ec93d-jupyter-master.dev-jhmtvbr7.svc.cluster.local>
2021-12-28 10:40:56 +0000

    update

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,29 +4,7 @@ import os
 
 
 
-def preprocess(in_path, output_path):
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    with open(in_path, 'r') as f:
-        lines = f.readlines()
-    total = len(lines)
-    print(f'file = {in_path}, size = {total}, output_path = {output_path}')
-    data = []
-    for line in tqdm(lines):
-        line = line.strip().split('\t')
-        context = '<sep>'.join(line[:-1]) + '<sep><extra_id_1>'
-        response = '<extra_id_1>' + line[-1]
-        data.append((context, response))
-
-    # split
-    random.shuffle(data)
-    train_size = int(total * 0.7)
-    test_size = int(total * 0.15)
-    train_data = data[: train_size]
-    val_data = data[train_size: -test_size]
-    test_data = data[-test_size: ]
-
+def preprocess(in_path, output_path, split=True):
     def dump_data(split, a):
         source, target = [], []
         for i in a:
@@ -40,21 +18,48 @@ def preprocess(in_path, output_path):
             for i in target:
                 f.write(i)
                 f.write('\n')
-    
-    dump_data('train', train_data)
-    dump_data('val', val_data)
-    dump_data('test', test_data)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    with open(in_path, 'r') as f:
+        lines = f.readlines()
+    total = len(lines)
+    print(f'file = {in_path}, size = {total}, output_path = {output_path}')
+    data = []
+    for line in tqdm(lines):
+        line = line.strip().split('\t\t')
+        context = '<sep>'.join(line[:-1]) + '<sep><extra_id_1>'
+        response = '<extra_id_1>' + line[-1]
+        data.append((context, response))
+
+    if split:
+        # split
+        random.shuffle(data)
+        train_size = int(total * 0.7)
+        test_size = int(total * 0.15)
+        train_data = data[: train_size]
+        val_data = data[train_size: -test_size]
+        test_data = data[-test_size: ]
+        
+        dump_data('train', train_data)
+        dump_data('val', val_data)
+        dump_data('test', test_data)
+    else:
+        dump_data('test', data)
 
 
 
 if __name__ == "__main__":
     random.seed(22)
 
-    files = [
-        # ('/dataset/f1d6ea5b/gyx-eva/data/multi/train.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/1.4G'),
-        # ('/dataset/f1d6ea5b/sunhao/merge_shuffled_multi_0925_4.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/merge_shuffled_multi_0925_4'),
-        ('/dataset/f1d6ea5b/sunhao/filter/merge_shuffled_0907.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/12G')
-    ]
+    # files = [
+    #     # ('/dataset/f1d6ea5b/gyx-eva/data/multi/train.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/1.4G'),
+    #     # ('/dataset/f1d6ea5b/sunhao/merge_shuffled_multi_0925_4.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/merge_shuffled_multi_0925_4'),
+    #     ('/dataset/f1d6ea5b/sunhao/filter/merge_shuffled_0907.txt', '/dataset/f1d6ea5b/wenjiaxin/lot/12G')
+    # ]
 
-    for i in files:
-        preprocess(i[0], i[1])
+    # for i in files:
+    #     preprocess(i[0], i[1])
+
+    preprocess('/dataset/f1d6ea5b/sunhao/EVA_test_0827/test_multi.txt', 'eva_test', split=False)
--- infer.sh
+++ infer.sh
@@ -1,11 +1,12 @@
 WORKING_DIR=/dataset/f1d6ea5b/wenjiaxin/lot
 
 
-CKPT_PATH=lot_large_esc_0828_data/checkpoint-5900
+# CKPT_PATH=lot_large_esc_0828_data/checkpoint-5900
+CKPT_PATH=1006_lot_large_1.4G/checkpoint-40000
 LOAD_PATH=${WORKING_DIR}/results/$CKPT_PATH
 
-INPUT_PATH=${WORKING_DIR}/esc/test
-OUTPUT_PATH=${LOAD_PATH}/infer_result/
+INPUT_PATH=${WORKING_DIR}/eva_test/test
+OUTPUT_PATH=${LOAD_PATH}/1006_infer_result/
 
 
 python code/infer.py \
